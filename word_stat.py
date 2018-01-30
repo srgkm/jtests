ee0b824d17f04dd81f2898272d3571f05cd98a51
Sergei Kim <srg.kim@gmail.com>
2018-01-30 15:22:43 +0300

    wip

---
+++ word_stat.py
@@ -0,0 +1,41 @@
+from collections import Counter, defaultdict
+import os
+import sys
+import re
+
+
+def main():
+    """
+    (Hello)
+
+    Naive implementation:
+
+    - We expect utf-8 encoded text files
+    - We expect mono lang text files, in case of multi lang alpha sorting can be weired
+    - We expect plain text (no HTML)
+    - No optimizations for reading/sorting very large files
+
+    Usage: python3 ./word_stat.py filename
+    """
+    try:
+        _, filename = sys.argv
+    except ValueError:
+        print('Error: bad args')
+        print('Usage: words_stat.py filename')
+        return
+    if not os.path.exists(filename):
+        print('Error: file %s does not exist' % filename)
+        return
+    with open(filename, mode='rt') as f:
+        text = f.read()
+        words = re.findall(r'[-\w]+', text.lower())
+        words_grouped_by_counter = defaultdict(list)
+        for word, counter in Counter(words).items():
+            words_grouped_by_counter[counter].append(word)
+        for group, words in sorted(words_grouped_by_counter.items(), key=lambda x: x[0], reverse=True):
+            for word in sorted(words):
+                print('%s: %s' % (word, group))
+
+
+if __name__ == '__main__':
+    main()
